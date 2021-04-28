95388a94d64c4233ded427c8f4e1d7a107c5e202
Cameron Downer <camerondowner@gmail.com>
2021-04-28 16:46:43 -0500

    implemented functionality to verify login credentials

--- authServer.js
+++ authServer.js
@@ -1,4 +1,10 @@
+const User = require('./routes/models/user');
 require('dotenv').config();
+const bcrypt = require('bcryptjs');
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true});
 
 const express = require('express');
 const app = express();
@@ -27,14 +33,40 @@ app.post('/token', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', verifyLogin, async (req, res) => {
     const username = req.body.username;
-    const user = { username: username };
+    const user = { username: username, };
 
     const accessToken = generateAccessToken(user);
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
     res.status(201).json({ accessToken: accessToken, refreshToken: refreshToken });
 })
+
+async function verifyLogin (req, res, next){
+    const username = req.body.username;
+    const password = req.body.password;
+    let user
+    try{
+        user = await User.findOne({username: username});
+        if(user == null){
+            return res.status(404).json({ message: 'User doesnt exist' });
+        }
+        else{
+            bcrypt.compare(password, user.passwordHash, function(err, isValid) {
+                if(isValid){
+                    next()
+                }
+                else{
+                    return res.status(401).json({ message: 'Invalid password' });
+                }
+              });
+        }
+    }
+    catch (error){
+        return res.status(500).json({ message: error.message })
+    }
+
+}
 
 function generateAccessToken(user){
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' })
